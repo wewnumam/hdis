9634f7594d231e6938ee18e08d8d0b832c4621d2
Ahmad Nuhisya <wvwnumam@gmail.com>
2020-01-31 08:39:27 +0700

    Membuat static file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port, () => console.log(`web app listening on port ${port}!`))
