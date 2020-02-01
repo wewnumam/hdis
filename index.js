46e5f5288579dd17f20dd33215881e087414d65a
Ahmad Nuhisya <wvwnumam@gmail.com>
2020-02-02 06:15:44 +0700

    Menambah handlebar, crud file, sb-admin

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 8000
+const express  = require('express')
+const path     = require('path')
+const xphbs    = require('express-handlebars')
+const app      = express()
+const port     = 8000
+const makeCrud = require('express-json-file-crud').makeCrud
+const pasien   = makeCrud('pasien', './data')
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
+// handlebars middleware
+app.engine('handlebars', xphbs({
+    defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
 
+// crud routes
+app.use('/pasien', pasien)
+
+// url controller
+app.get('/', function (req, res) {
+    res.render('dashboard');
+});
+
+// static file
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => console.log(`web app listening on port ${port}!`))
